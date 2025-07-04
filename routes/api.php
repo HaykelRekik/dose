dc2c059503471035a8c3ffc4f6c66f9da52c9d16
MohamedAmineOuerheni <m.ouerheni@atsu-it.com>
2025-07-04 13:11:30 +0100

    feat : add request otp and verify otp routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use App\Http\Controllers\API\Auth\OTPController;
 use App\Http\Controllers\API\CategoryController;
 use App\Http\Controllers\API\ProductController;
 use Illuminate\Support\Facades\Route;
@@ -22,4 +23,12 @@ Route::prefix('categories')->group(function (): void {
 Route::prefix('products')->group(function (): void {
     Route::get('/', [ProductController::class, 'index']);
     Route::get('/{product}', [ProductController::class, 'show']);
+});
+
+Route::prefix('auth')->group(function (): void {
+    // OTP - Login //
+    Route::prefix('otp')->middleware('throttle:5,1')->group(function (): void {
+        Route::post('request', [OTPController::class, 'requestOtp']);
+        Route::post('verify', [OTPController::class, 'verifyOtp']);
+    });
 });
