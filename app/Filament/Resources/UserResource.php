fa4bcfdc5174250eb7e19bf9126ad6e20f2492b6
Haykel Rekik <hayk.rekik@gmail.com>
2025-07-08 06:19:44 +0100

    style: pint

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -7,7 +7,6 @@ namespace App\Filament\Resources;
 use App\Enums\UserRole;
 use App\Filament\Resources\UserResource\Pages;
 use App\Models\User;
-use Filament\Facades\Filament;
 use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Pages\Page;
@@ -52,14 +51,14 @@ class UserResource extends Resource
                             ->schema([
                                 Forms\Components\TextInput::make('email')
                                     ->label(__('Email'))
-                                    ->required(fn(Forms\Get $get) => $get('role') !== UserRole::CUSTOMER->value)
-                                    ->hidden(fn(Forms\Get $get) => $get('role') === UserRole::CUSTOMER->value)
+                                    ->required(fn (Forms\Get $get) => $get('role') !== UserRole::CUSTOMER->value)
+                                    ->hidden(fn (Forms\Get $get) => $get('role') === UserRole::CUSTOMER->value)
                                     ->email()
                                     ->unique(ignoreRecord: true),
                                 Forms\Components\TextInput::make('password')
                                     ->label(__('Password'))
-                                    ->required(fn(Forms\Get $get) => $get('role') !== UserRole::CUSTOMER->value)
-                                    ->hidden(fn(Forms\Get $get) => $get('role') === UserRole::CUSTOMER->value)
+                                    ->required(fn (Forms\Get $get) => $get('role') !== UserRole::CUSTOMER->value)
+                                    ->hidden(fn (Forms\Get $get) => $get('role') === UserRole::CUSTOMER->value)
                                     ->password()
                                     ->visibleOn('create')
                                     ->revealable(),
@@ -71,8 +70,8 @@ class UserResource extends Resource
                                     )
                                     ->searchable()
                                     ->preload()
-                                    ->required(fn(Forms\Get $get) => $get('role') === UserRole::EMPLOYEE->value)
-                                    ->visible(fn(Forms\Get $get) => $get('role') === UserRole::EMPLOYEE->value),
+                                    ->required(fn (Forms\Get $get) => $get('role') === UserRole::EMPLOYEE->value)
+                                    ->visible(fn (Forms\Get $get) => $get('role') === UserRole::EMPLOYEE->value),
                             ]),
                     ]),
             ]);
@@ -91,7 +90,7 @@ class UserResource extends Resource
                     ->icon('phosphor-envelope-open-duotone')
                     ->placeholder(__('Not specified'))
                     ->searchable()
-                    ->visible(fn(Page $livewire): bool => 'customers' !== $livewire->activeTab),
+                    ->visible(fn (Page $livewire): bool => 'customers' !== $livewire->activeTab),
                 PhoneColumn::make('phone')
                     ->label(__('Phone'))
                     ->icon('phosphor-phone-call-duotone')
@@ -100,11 +99,11 @@ class UserResource extends Resource
                 Tables\Columns\TextColumn::make('role')
                     ->label(__('Account Type'))
                     ->badge()
-                    ->hidden(fn(Page $livewire): bool => 'all' !== $livewire->activeTab),
+                    ->hidden(fn (Page $livewire): bool => 'all' !== $livewire->activeTab),
                 Tables\Columns\TextColumn::make('branch.name_' . app()->getLocale())
                     ->label(__('Branch'))
                     ->weight(FontWeight::Medium)
-                    ->hidden(fn(Page $livewire): bool => 'employees' !== $livewire->activeTab),
+                    ->hidden(fn (Page $livewire): bool => 'employees' !== $livewire->activeTab),
                 Tables\Columns\TextColumn::make('last_login_at')
                     ->label(__('Last login at'))
                     ->dateTime()
@@ -158,6 +157,4 @@ class UserResource extends Resource
     {
         return __('Users Management');
     }
-
-
 }
--- routes/api.php
+++ routes/api.php
@@ -6,14 +6,6 @@ use App\Http\Controllers\API\BranchController;
 use App\Http\Controllers\API\CategoryController;
 use App\Http\Controllers\API\ProductController;
 
-Route::get('/test', fn () => response()->json(
-    [
-        'version' => app()->version(),
-        'locale' => app()->getLocale(),
-        'text' => __('http-statuses.0'),
-    ]
-));
-
 Route::prefix('categories')->group(function (): void {
     Route::get('/', [CategoryController::class, 'index']);
     Route::get('/{category:slug}', [CategoryController::class, 'show']);
